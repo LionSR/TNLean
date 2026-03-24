6ffc3c8b816e7d310d745b03a1ec3a49023085d5
Ray <divergence_lu@icloud.com>
2026-03-24 18:18:36 +0100

    Use shared orthogonal projection PSD lemma in FromSpectral

--- TNLean/Channel/Irreducible/FromSpectral.lean
+++ TNLean/Channel/Irreducible/FromSpectral.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 TNLean contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 import TNLean.Channel.Irreducible.Ergodicity
+import TNLean.Channel.Irreducible.Basic
 import TNLean.Channel.Irreducible.SpectralRadius
 import TNLean.Channel.Irreducible.TraceAdjoint
 
