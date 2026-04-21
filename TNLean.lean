2bf613380dcbc78d15141db593cc75caaed1227b
Sirui@MPQ <divergence_lu@icloud.com>
2026-04-21 08:16:11 +0200

    feat(Channel/FixedPoint): add Choi-Effros identities for idempotent maps (#612)

--- TNLean.lean
+++ TNLean.lean
@@ -95,6 +95,7 @@ import TNLean.Channel.Schwarz.MultiplicativeDomainPowers
 import TNLean.Channel.Schwarz.MultiplicativeDomainFull
 import TNLean.Channel.Schwarz.TraceCFC
 import TNLean.Channel.FixedPoint.Algebra
+import TNLean.Channel.FixedPoint.ChoiEffros
 import TNLean.Channel.FixedPoint.Cesaro
 import TNLean.Channel.FixedPoint.StationarySupport
 import TNLean.Channel.FixedPoint.WedderburnDecomp
