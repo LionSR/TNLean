b7e16e99b932f3a763cd7c1874c050c8ed9d3854
Sirui Lu <lion.sr@icloud.com>
2026-05-11 17:04:01 +0200

    feat(MPS/FT): add nondecaying overlap partner uniqueness

--- TNLean/MPS/FundamentalTheorem/Full/NondecayingOverlap.lean
+++ TNLean/MPS/FundamentalTheorem/Full/NondecayingOverlap.lean
@@ -2,7 +2,7 @@
 Copyright (c) 2025 TNLean contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
-import TNLean.MPS.FundamentalTheorem.Full.ProportionalDominant
+import TNLean.MPS.FundamentalTheorem.Full.NondecayingPartnerUnique
 
 /-!
 # Non-decaying overlap existence for BNT families
@@ -54,6 +54,7 @@ open Filter
 namespace MPSTensor
 
 section HeteroEqualCase
+
 
 /-- **Non-decaying overlap existence for equal-MPV BNT families.**
 
