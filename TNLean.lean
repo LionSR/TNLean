3ed456eea1432176f176520a59382fa34f9df81a
Ray <divergence_lu@icloud.com>
2026-05-17 11:06:11 +0200

    cleanup(MPS/CanonicalForm): delete dead FromPrimitive/FromPeripheralPrimitive + retire unused BNTGrouping decls (wave 2A) (#1757)

--- TNLean.lean
+++ TNLean.lean
@@ -211,7 +211,6 @@ import TNLean.MPS.BNT.PermutationRigidityPrimitive
 import TNLean.MPS.BNT.Construction
 import TNLean.MPS.Structure.PrimitivityBridge
 import TNLean.MPS.Overlap.PeripheralToSpectralGap
-import TNLean.MPS.CanonicalForm.FromPrimitive
 import TNLean.MPS.FundamentalTheorem.Multi
 import TNLean.MPS.FundamentalTheorem.SectorWeightComparison
 import TNLean.MPS.FundamentalTheorem.SectorDecomposition
