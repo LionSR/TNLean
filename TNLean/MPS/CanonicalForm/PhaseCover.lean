c250c393f5478997d8fd2894fc03d29306e5dde9
Ray <divergence_lu@icloud.com>
2026-05-21 20:32:47 +0200

    chore(MPS/BNT): split separation predicates
    Move lightweight BNT separation and block-matching conclusion shapes into a small module, keeping construction imports explicit where construction lemmas are used.

--- TNLean/MPS/CanonicalForm/PhaseCover.lean
+++ TNLean/MPS/CanonicalForm/PhaseCover.lean
@@ -2,7 +2,7 @@
 Copyright (c) 2026 TNLean contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
-import TNLean.MPS.BNT.Construction
+import TNLean.MPS.BNT.Separation
 import TNLean.MPS.Overlap.CastLemmas
 import TNLean.MPS.SharedInfra.GaugePhase
 import TNLean.MPS.SharedInfra.SectorDecomposition
