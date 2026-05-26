bd43a30af64f2c672bdc4ed60f2b901eb822c7c2
Sirui Lu <lion.sr@icloud.com>
2026-05-26 21:52:50 +0200

    refactor(PEPS): split translated edge blockings

--- TNLean.lean
+++ TNLean.lean
@@ -369,6 +369,7 @@ import TNLean.PEPS.NormalBlocking
 import TNLean.PEPS.SquareLatticeGraph
 import TNLean.PEPS.NormalEdgeComplementCover
 import TNLean.PEPS.NormalEdgeBlockingCoordinate
+import TNLean.PEPS.NormalEdgeBlockingTranslated
 import TNLean.PEPS.IdentityInsertion
 import TNLean.PEPS.InsertionRealization
 import TNLean.PEPS.LocalGauge
