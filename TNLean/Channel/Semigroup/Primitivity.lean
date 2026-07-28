a400e71e7699773831ac3625f7188afcfd2bac72
texra-ai <divergence_lu@icloud.com>
2026-07-28 02:46:34 +0200

    fix(imports): restore umbrella module contracts

--- TNLean/Channel/Semigroup/Primitivity.lean
+++ TNLean/Channel/Semigroup/Primitivity.lean
@@ -3,3 +3,7 @@ Copyright (c) 2026 TNLean contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: TNLean contributors
 -/
+import TNLean.Channel.Semigroup.Primitivity.Basic
+import TNLean.Channel.Semigroup.Primitivity.Helpers
+import TNLean.Channel.Semigroup.Primitivity.IrreducibleAnalysis
+import TNLean.Channel.Semigroup.Primitivity.MainTheorem
--- TNLean/Channel/Semigroup/ReducibleQDS.lean
+++ TNLean/Channel/Semigroup/ReducibleQDS.lean
@@ -3,3 +3,8 @@ Copyright (c) 2026 TNLean contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: TNLean contributors
 -/
+import TNLean.Channel.Semigroup.ReducibleQDS.Defs
+import TNLean.Channel.Semigroup.ReducibleQDS.FixedDensity
+import TNLean.Channel.Semigroup.ReducibleQDS.GeneratorCompression
+import TNLean.Channel.Semigroup.ReducibleQDS.SubsequenceAnalysis
+import TNLean.Channel.Semigroup.ReducibleQDS.Equivalence
