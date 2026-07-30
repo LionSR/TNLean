b1f91a1821f580df33fc077b8954133f721ddad4
Sirui Lu <divergence_lu@icloud.com>
2026-07-30 06:59:32 +0200

    feat(MPS/MPDO): prove sector-chain fiber contraction (#5139)

--- TNLean/MPS/MPDO.lean
+++ TNLean/MPS/MPDO.lean
@@ -222,6 +222,7 @@ import TNLean.MPS.MPDO.RepresentativeGroupedMarkedLemmaL
 import TNLean.MPS.MPDO.RetainedClass
 import TNLean.MPS.MPDO.SALArbitraryCut
 import TNLean.MPS.MPDO.SALPhysicalSectorCyclicPositivity
+import TNLean.MPS.MPDO.SectorChainFiberContraction
 import TNLean.MPS.MPDO.SectorCompressionSeparation
 import TNLean.MPS.MPDO.SectorEtaContraction
 import TNLean.MPS.MPDO.SectorEtaOperator
