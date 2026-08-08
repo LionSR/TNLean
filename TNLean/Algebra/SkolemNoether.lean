a7801495fc727589f3176dc367db4b9be800b868
texra-ai <texra-ai@users.noreply.github.com>
2026-08-08 19:33:15 +0200

    refactor(Algebra): decouple Skolem-Noether from MPS

--- TNLean/Algebra/SkolemNoether.lean
+++ TNLean/Algebra/SkolemNoether.lean
@@ -3,10 +3,10 @@ Copyright (c) 2026 TNLean contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: TNLean contributors
 -/
-import TNLean.MPS.Defs
-
-import Mathlib.LinearAlgebra.GeneralLinearGroup.AlgEquiv
+import Mathlib.Data.Complex.Basic
 import Mathlib.LinearAlgebra.Dimension.Finrank
+import Mathlib.LinearAlgebra.GeneralLinearGroup.AlgEquiv
+import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.Defs
 import Mathlib.RingTheory.SimpleRing.Matrix
 
 /-!
