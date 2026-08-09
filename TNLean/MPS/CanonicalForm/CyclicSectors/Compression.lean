79d06f96018a66707fb220348f62c94d1dc56014
texra-ai <texra-ai@users.noreply.github.com>
2026-08-10 00:20:49 +0200

    style: sort corner compression imports

--- TNLean/MPS/CanonicalForm/CyclicSectors/Compression.lean
+++ TNLean/MPS/CanonicalForm/CyclicSectors/Compression.lean
@@ -3,8 +3,8 @@ Copyright (c) 2026 TNLean contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: TNLean contributors
 -/
-import TNLean.Channel.Peripheral.CyclicDecomposition
 import TNLean.Channel.KrausCornerCompression
+import TNLean.Channel.Peripheral.CyclicDecomposition
 
 /-!
 # Compression to cyclic sectors
