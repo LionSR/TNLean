1dc1fa401e9443becfe24f437898d25fe27b6ce8
texra-ai <texra-ai@users.noreply.github.com>
2026-08-31 21:05:26 +0200

    style(parent-hamiltonian): sort Knabe foundation imports

--- TNLean/MPS/ParentHamiltonian/Martingale/CyclicWindowOpenHamiltonian.lean
+++ TNLean/MPS/ParentHamiltonian/Martingale/CyclicWindowOpenHamiltonian.lean
@@ -3,10 +3,10 @@ Copyright (c) 2026 TNLean contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: TNLean contributors
 -/
+import QICLean.Analysis.FiniteRangeKnabe
 import TNLean.MPS.ParentHamiltonian.CyclicWindowIndex
 import TNLean.MPS.ParentHamiltonian.Martingale.OpenHamiltonian
 import TNLean.MPS.ParentHamiltonian.Martingale.SpectatorTransport
-import QICLean.Analysis.FiniteRangeKnabe
 
 /-!
 # Cyclic windows as open parent Hamiltonians
