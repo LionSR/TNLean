5130531f7bf1b3c92ce672836e3a13951cce3982
texra-ai <texra-ai@outlook.com>
2026-09-26 08:20:37 +0200

    docs(MPS/MPU): align parity witness dependencies

--- TNLean/MPS/MPU/StandardFormParityCounterexample.lean
+++ TNLean/MPS/MPU/StandardFormParityCounterexample.lean
@@ -3,8 +3,8 @@ Copyright (c) 2026 TNLean contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: TNLean contributors
 -/
+import TNLean.MPS.MPDO.IdentityTensor
 import TNLean.MPS.MPDO.SimpleScaling
-import TNLean.MPS.MPDO.StackedLayers
 import TNLean.MPS.MPU.Simple
 
 /-!
